734234f23ce18a750dbac77ce7135ec7fc7d9490
Aadya <aaadya_be22@thapar.edu>
2024-10-22 23:35:47 +0530

    landing

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,20 @@
 import React from "react";
-function Navbar() {
+function Navbar({ page }) {
+    let buttonText;
+    switch(page) {
+        case 'Landing':
+        case 'Signup':
+            buttonText = 'Login';
+            break;
+        case 'Login':
+            buttonText = 'Signup';
+            break;
+        case 'Dashboard':
+            buttonText = 'Logout';
+            break;
+        default:
+            buttonText = 'Login'; // default value
+    }
     return (
         <>
         <div className="m-0 text-wht w-screen h-[8vh] flex flex-row items-center py-12 px-20 justify-between">
@@ -12,7 +27,7 @@ function Navbar() {
             </div>
 
             </div>
-            <div className="color-wht text-[1rem] px-6 py-1.5 font-semibold border-[0.5px] border-wht border-opacity-50">LOGIN</div>
+            <div className="text-[1rem] px-6 py-1.5 font-semibold border-[0.5px] border-wht border-opacity-50"> {buttonText}</div>
         </div>
         </>
     );
