f6e79681ef3dad725cd789d67569e900403bd4ad
Jas-4484 <jsingh11_be22@thapar.edu>
2024-10-23 14:24:41 +0530

    Dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,12 @@ import Login from "./pages/Login.jsx";
 import Landing from "./pages/Landing.jsx";
 import About from "./pages/About.jsx";
 import Events from "./pages/Events.jsx";
+<<<<<<< Updated upstream
 import Signup from "./pages/Signup.jsx";
+=======
+import Dashboard from "./pages/Dashboard.jsx";
+
+>>>>>>> Stashed changes
 function App() {
   return (
     <>
@@ -12,7 +17,11 @@ function App() {
         <Route path="/login" element={<Login/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/events' element={<Events/>} />
+<<<<<<< Updated upstream
         <Route path="/signup" element={<Signup/>} />
+=======
+        <Route path="/dashboard" element={<Dashboard/>} />
+>>>>>>> Stashed changes
       </Routes>
     </>
   );
