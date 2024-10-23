476f2ebc485c7c149eaf61abcf40cdc61b801fbf
Aadya <aaadya_be22@thapar.edu>
2024-10-23 14:27:04 +0530

    change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,25 +3,18 @@ import Login from "./pages/Login.jsx";
 import Landing from "./pages/Landing.jsx";
 import About from "./pages/About.jsx";
 import Events from "./pages/Events.jsx";
-<<<<<<< Updated upstream
-import Signup from "./pages/Signup.jsx";
-=======
 import Dashboard from "./pages/Dashboard.jsx";
-
->>>>>>> Stashed changes
+import Signup from "./pages/Signup.jsx";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/events' element={<Events/>} />
-<<<<<<< Updated upstream
-        <Route path="/signup" element={<Signup/>} />
-=======
         <Route path="/dashboard" element={<Dashboard/>} />
->>>>>>> Stashed changes
       </Routes>
     </>
   );
