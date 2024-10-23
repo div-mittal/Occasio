a976c7116163f5f33014338868c800f64d68dac2
Aadya <aaadya_be22@thapar.edu>
2024-10-23 23:31:05 +0530

    attendee+organizer

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -17,14 +17,14 @@ function Landing() {
             <Navbar/>
             <div className="mx-20 ml-12 my-16 mb-0  h-[55vh] flex items-center ">
                 <div className="px-32 py-20 h-[100%] w-[50%] flex flex-col gap-[3rem] text-wht border-r border-wht border-{0.5px} border-opacity-50 height-100%">
-                    <h1 className="text-xl">
+                    <h1 className="cursor-default text-xl">
                     Effortlessly organize, manage, and track attendance for conferences, workshops, and private gatherings. Create events, send invites, manage RSVPs, and track guest preferences—all in one place.
                     </h1>
                     <div className="flex flex-col gap-[1.5rem]">
-                        <div className="text-[1.15rem]  font-semibold">Sign Up below to begin</div>
+                        <div className="text-[1.15rem] cursor-default font-semibold">Sign Up below to begin</div>
                         <div className="flex gap-[1.5rem]">
-                            <Button onClick={() => navigate('/Signup')} type="primary" size='large' className='font-bold text-blk hover:!text-blk'>Attendee</Button>
-                            <Button type="primary" size='large' className='font-bold text-blk hover:!text-blk'>Organizer</Button>
+                            <Button onClick={() => navigate('/Signup', { state: { role: 'Attendee' }})} type="primary" size='large' className='font-bold text-blk hover:!text-blk'>Attendee</Button>
+                            <Button onClick={() => navigate('/Signup', { state: { role: 'Organizer' }})} type="primary" size='large' className='font-bold text-blk hover:!text-blk'>Organizer</Button>
                             {/* <div className="text-[1.1rem] bg-ylw text-blk px-6 py-1.5 font-semibold rounded">Attendee</div> */}
                             {/* <div className="text-[1.1rem] bg-ylw text-blk px-6 py-1.5 font-semibold  rounded">Organizer</div> */}
                         </div>
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from '../components/Navbar'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { Button,ConfigProvider} from "antd";
 const Signup = () => {
     const navigate = useNavigate(); 
+    const location = useLocation();
+    const [role, setRole] = useState(location.state?.role || 'Attendee');
 return (
     <div className='bg-blk h-screen text-wht'>
         <ConfigProvider theme={{
@@ -12,24 +14,62 @@ return (
           borderRadius: 2,
         },
       }}>
-            <Navbar page='Login'/>
+            <Navbar page='Signup'/>
             <div className='flex justify-between mx-20 my-5'>
             <button onClick={() => navigate('/')} className='cursor-pointer text-[1.2rem]'>&lt; Back</button>
             <div className='flex gap-[6px] cursor-pointer hover:text-ylw hover:font-semibold'>
-                <div>Organizer?</div>
-                <div className='hover:text-wht hover:font-normal' >Click Here</div>
+                <div>{role=="Attendee" ? "Organizer?" : "Attendee?"}</div>
+                <div className='hover:text-wht hover:font-normal' onClick={()=>{
+                    setRole(
+                        role == "Attendee" ? "Organizer" : "Attendee"
+                    )
+                }} >Click Here</div>
             </div>
             </div>
             <div className='flex h-[55vh] ml-20 mr-10 mt-5'>
                 <div className='p-5 px-6 rounded border-wht border h-full w-[55%] flex flex-col'>
-                    <div className='text-[1.4rem] flex justify-between w-full'>
+                    <div className='cursor-default text-[1.4rem] flex justify-between w-full'>
                         <div>Sign Up</div>
-                        <div className='opacity-25'>Attendee</div>
+                        <div className='opacity-25'>{role == "Attendee" ? "Attendee" : "Organizer"}</div>
                     </div>
-                    <div className='h-[75%] my-2'></div>
+                    {
+                        role==="Attendee" && 
+                        <div className='h-[75%] my-8 flex flex-col gap-[1rem]'>
+                        <div className='flex gap-[50px]'>
+                        <input type="text" placeholder="First Name" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2 ' />
+                        <input type="text" placeholder="Last Name" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2' />
+                        </div>
+                        <div className='flex gap-[50px]'>
+                        <input type="text" placeholder="Email" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2 ' />
+                        <input type="password" placeholder="Password" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2' />
+                        </div>
+                        <div className='flex gap-[50px]'>
+                        <input type="text" placeholder="Phone Number" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2 ' />
+                        <input type="text" placeholder="Address" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2' />
+                        </div>
+                        
+                        </div>
+                    }
+                    {
+                        role==="Organizer" &&
+                        <div className='h-[75%] my-8 flex flex-col gap-[1rem]'>
+                        <div className='flex gap-[50px]'>
+                        <input type="text" placeholder="First Name" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2 ' />
+                        <input type="text" placeholder="Last Name" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2' />
+                        </div>
+                        <div className='flex gap-[50px]'>
+                        <input type="text" placeholder="Email" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2 ' />
+                        <input type="password" placeholder="Password" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2' />
+                        </div>
+                        <div className='flex gap-[50px]'>
+                        <input type="text" placeholder="Phone Number" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2 ' />
+                        <input type="text" placeholder="Address" className='w-[50%] bg-transparent p-2 border border-wht border-opacity-25 rounded my-2' />
+                        </div>
+                        
+                        </div>
+                    }
                     <div className='flex justify-end'>
-                    {/* <button className=''>Register</button> */}
-                    <Button onClick={() => navigate('/Signup')} type="primary" size='large' className='font-bold text-blk hover:!text-blk'>Register</Button>
+                    <Button onClick={() => navigate('/Dashboard')} type="primary" size='large' className='font-bold text-blk hover:!text-blk'>Register</Button>
                     </div>
                 </div>
                 <img className='w-[40%] h-[140%] mt-[-5%]' src="cal.png" alt="" />
