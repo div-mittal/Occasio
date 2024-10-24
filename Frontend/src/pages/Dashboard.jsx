aa440ea5b0a00978ac778c122b7fed174538fd34
Jas-4484 <jsingh11_be22@thapar.edu>
2024-10-24 14:03:18 +0530

    Cards

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -6,17 +6,20 @@ const Dashboard = () => {
         <div className='h-[100vh] bg-blk flex flex-col'>
             <Navbar/>
             <div className='dash w-full px-20 py-8 flex-grow'>
-                <div className='flex flex-col w-full h-full p-6 border border-solid border-wht border-opacity-25 rounded-lg'>
+                <div className='relative flex flex-col w-full h-full p-6 border border-solid border-wht border-opacity-25 rounded-lg'>
                     <h1 className='text-wht text-2xl'>Dashboard</h1>
                     <div className='event-list flex flex-row flex-grow gap-4 py-4 overflow-auto minimal-scrollbar'>
                     <Ecard/>
                     <Ecard/>
                     <Ecard/>
+                    <Ecard/><Ecard/>
+                    <Ecard/><Ecard/>
+                    <Ecard/><Ecard/>
+                    <Ecard/><Ecard/>
                     <Ecard/>
-                    <Ecard/>
-                    <Ecard/>
-                    <Ecard/>
-                    <Ecard/>
+                    </div>
+                    <div className='flex justify-center items-center h-12 w-12 bg-ylw rounded-md absolute bottom-16 right-12'>
+                        <h1 className='text-4xl font-bold text-blk'>+</h1>
                     </div>
                 </div>
             </div>
